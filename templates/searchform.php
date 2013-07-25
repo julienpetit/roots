3bf9626433e47da8158144de24d2da51eb6b0851
Julien Petit <Julien@jpetits-MacBook-Pro.local>
2013-07-25 17:58:59 +0200

    wip

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,5 +1,5 @@
 <div id="search">	
-	<form method="get" id="searchform" action="http://wordpress.dev/">
+	<form method="get" id="searchform" action="/">
 		<label for="s" class="assistive-text">Search</label>
 		<input type="text" class="field" name="s" id="s" placeholder="recherche">
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="Search">
