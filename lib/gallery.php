0d984b9ca148dc0299536f0afaf3ed65e1853183
Julien Petit <Julien@jpetits-MacBook-Pro.local>
2013-07-25 16:29:31 +0200

    wip

--- lib/gallery.php
+++ lib/gallery.php
@@ -6,99 +6,99 @@
  *
  * @link http://twitter.github.com/bootstrap/components.html#thumbnails
  */
-function roots_gallery($attr) {
-  $post = get_post();
+// function roots_gallery($attr) {
+//   $post = get_post();
 
-  static $instance = 0;
-  $instance++;
+//   static $instance = 0;
+//   $instance++;
 
-  if (!empty($attr['ids'])) {
-    if (empty($attr['orderby'])) {
-      $attr['orderby'] = 'post__in';
-    }
-    $attr['include'] = $attr['ids'];
-  }
+//   if (!empty($attr['ids'])) {
+//     if (empty($attr['orderby'])) {
+//       $attr['orderby'] = 'post__in';
+//     }
+//     $attr['include'] = $attr['ids'];
+//   }
 
-  $output = apply_filters('post_gallery', '', $attr);
+//   $output = apply_filters('post_gallery', '', $attr);
 
-  if ($output != '') {
-    return $output;
-  }
+//   if ($output != '') {
+//     return $output;
+//   }
 
-  if (isset($attr['orderby'])) {
-    $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
-    if (!$attr['orderby']) {
-      unset($attr['orderby']);
-    }
-  }
+//   if (isset($attr['orderby'])) {
+//     $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+//     if (!$attr['orderby']) {
+//       unset($attr['orderby']);
+//     }
+//   }
 
-  extract(shortcode_atts(array(
-    'order'      => 'ASC',
-    'orderby'    => 'menu_order ID',
-    'id'         => $post->ID,
-    'itemtag'    => '',
-    'icontag'    => '',
-    'captiontag' => '',
-    'columns'    => 3,
-    'size'       => 'thumbnail',
-    'include'    => '',
-    'exclude'    => '',
-    'link'       => 'file'
-  ), $attr));
+//   extract(shortcode_atts(array(
+//     'order'      => 'ASC',
+//     'orderby'    => 'menu_order ID',
+//     'id'         => $post->ID,
+//     'itemtag'    => '',
+//     'icontag'    => '',
+//     'captiontag' => '',
+//     'columns'    => 3,
+//     'size'       => 'thumbnail',
+//     'include'    => '',
+//     'exclude'    => '',
+//     'link'       => 'file'
+//   ), $attr));
 
-  $id = intval($id);
+//   $id = intval($id);
 
-  if ($order === 'RAND') {
-    $orderby = 'none';
-  }
+//   if ($order === 'RAND') {
+//     $orderby = 'none';
+//   }
 
-  if (!empty($include)) {
-    $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+//   if (!empty($include)) {
+//     $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
 
-    $attachments = array();
-    foreach ($_attachments as $key => $val) {
-      $attachments[$val->ID] = $_attachments[$key];
-    }
-  } elseif (!empty($exclude)) {
-    $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
-  } else {
-    $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
-  }
+//     $attachments = array();
+//     foreach ($_attachments as $key => $val) {
+//       $attachments[$val->ID] = $_attachments[$key];
+//     }
+//   } elseif (!empty($exclude)) {
+//     $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+//   } else {
+//     $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+//   }
 
-  if (empty($attachments)) {
-    return '';
-  }
+//   if (empty($attachments)) {
+//     return '';
+//   }
 
-  if (is_feed()) {
-    $output = "\n";
-    foreach ($attachments as $att_id => $attachment) {
-      $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
-    }
-    return $output;
-  }
-  // $output = '<div class="container-fluid">';
-  $output .= '<div class="row-fluid">';
-  $output .= '<ul class="thumbnails gallery">';
+//   if (is_feed()) {
+//     $output = "\n";
+//     foreach ($attachments as $att_id => $attachment) {
+//       $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+//     }
+//     return $output;
+//   }
+//   // $output = '<div class="container-fluid">';
+//   $output .= '<div class="row-fluid">';
+//   $output .= '<ul class="thumbnails gallery">';
 
-  $i = 0;
-  foreach ($attachments as $id => $attachment) {
-    $image = ('file' == $link) ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+//   $i = 0;
+//   foreach ($attachments as $id => $attachment) {
+//     $image = ('file' == $link) ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
 
-    $image = str_replace("width=\"250\"", " ", $image);
-    $image = str_replace("height=\"250\"", " ", $image);
-    $output .= '<li class="span4">' . $image;
-    if (trim($attachment->post_excerpt)) {
-      $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
-    }
-    $output .= '</li>';
-  }
+//     $image = str_replace("width=\"250\"", " ", $image);
+//     $image = str_replace("height=\"250\"", " ", $image);
+//     $output .= '<li class="span4">' . $image;
+//     if (trim($attachment->post_excerpt)) {
+//       $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
+//     }
+//     $output .= '</li>';
+//   }
 
-  $output .= '</ul>';
-  $output .= '</div>';
-  // $output .= '</div>';
-  return $output;
-}
-if (current_theme_supports('bootstrap-gallery')) {
-  remove_shortcode('gallery');
-  add_shortcode('gallery', 'roots_gallery');
-}
+//   $output .= '</ul>';
+//   $output .= '</div>';
+//   // $output .= '</div>';
+//   return $output;
+// }
+// if (current_theme_supports('bootstrap-gallery')) {
+//   remove_shortcode('gallery');
+//   add_shortcode('gallery', 'roots_gallery');
+// }
