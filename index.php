c9abdb21b1ab5c3c3a61f635bdf30e4e83ef7c56
Julien Petit <Julien@jpetits-MacBook-Pro.local>
2013-09-14 01:12:20 +0200

    wip

--- index.php
+++ index.php
@@ -57,8 +57,8 @@ foreach ($posts as $key => $post) {
 <?php if ($wp_query->max_num_pages > 1) : ?>
   <nav class="post-nav">
     <ul class="pager">
-      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+      <li class="next"><?php next_posts_link(__('Page Suivante &rarr;', 'roots')); ?></li>
+      <li class="previous"><?php previous_posts_link(__('&larr; Page précédente', 'roots')); ?></li>
     </ul>
   </nav>
 <?php endif; ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,12 @@
-<footer id="footer" class="content-info" role="contentinfo">
+<footer id="footer" class="content-info container-fluid" role="contentinfo">
+
   <div class="container">
+    <div class="row">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
+  </div>
+
+  <div class="container">
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   </div>
 </footer>
