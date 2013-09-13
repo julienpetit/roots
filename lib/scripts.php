b5576d7de4150e620a5ec8dabf11e8ca3254e486
Julien Petit <Julien@jpetits-MacBook-Pro.local>
2013-09-14 01:45:06 +0200

    wip

--- lib/scripts.php
+++ lib/scripts.php
@@ -35,13 +35,13 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
-  wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('modernizr');
-  wp_enqueue_script('roots_plugins');
-  wp_enqueue_script('roots_main');
+  // wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
+  // wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
+  // wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
+  // wp_enqueue_script('jquery');
+  // wp_enqueue_script('modernizr');
+  // wp_enqueue_script('roots_plugins');
+  // wp_enqueue_script('roots_main');
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
--- templates/footer.php
+++ templates/footer.php
@@ -11,4 +11,11 @@
   </div>
 </footer>
 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri() . "/assets/js/vendor/modernizr-2.6.2.min.js"; ?>'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri() . "/assets/js/plugins.js"; ?>'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri() . "/assets/js/main.js"; ?>'></script>
+
 <?php wp_footer(); ?>
+
+
