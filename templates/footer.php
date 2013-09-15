b6dffc1013c07fd157e3933e0dd473fd395ca09d
Julien Petit <Julien@jpetits-MacBook-Pro.local>
2013-09-15 16:54:47 +0200

    wip

--- templates/footer.php
+++ templates/footer.php
@@ -11,11 +11,23 @@
   </div>
 </footer>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=528164940566303";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri() . "/assets/js/vendor/modernizr-2.6.2.min.js"; ?>'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri() . "/assets/js/plugins.js"; ?>'></script>
 <script type='text/javascript' src='<?php echo get_template_directory_uri() . "/assets/js/main.js"; ?>'></script>
 
 <?php wp_footer(); ?>
+
+
 
 
